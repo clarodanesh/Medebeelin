4f4231f71a73da911f0e03bf7218d90b7af5723b
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-03-23 22:57:35 +0000

    spritesheet for player added and animated, progress with level 1 design

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D playerRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerRigidBody.velocity = gameObject.transform.right * 10;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRigidBody.velocity = -gameObject.transform.right * 10;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerRigidBody.velocity = gameObject.transform.up * 10;
+        }
+    }
+}
