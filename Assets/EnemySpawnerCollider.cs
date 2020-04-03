a5c232189229433281af768a76604b1e05d2a3a7
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-04-03 02:46:01 +0100

    animations for character change on direction and speed

---
+++ Assets/EnemySpawnerCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerCollider : MonoBehaviour
+{
+    public GameObject bee;
+    System.Random rnd = new System.Random();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("Player"))
+        {
+            /*GameObject go = Instantiate(bee);
+            float randX = Random.Range(-10f, 10f);
+            float randY = Random.Range(-10f, 10f);
+            //go.transform.position = new Vector3(randX, randY, randX);
+            go.transform.position = new Vector3(-0.72f, -3.2f, 0f);*/
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 public class PlayerScript : MonoBehaviour
 {
     public ParticleSystem nectarHitParticles;
+    private int direction;
     public static int health;
     public bool isGrounded;
     public Rigidbody2D playerRigidBody;
+    public Animator animator;
+    public RuntimeAnimatorController rightAnim;
+    public RuntimeAnimatorController leftAnim;
+    public RuntimeAnimatorController stoppedRight;
+    public RuntimeAnimatorController stoppedLeft;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Enemy")
@@ -39,6 +45,8 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        direction = 1;
     }
 
     void CheckPlayerHealth()
@@ -55,17 +63,29 @@ public class PlayerScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            direction = 1;
             playerRigidBody.velocity = gameObject.transform.right * 7;
+            animator.runtimeAnimatorController = rightAnim as RuntimeAnimatorController;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            direction = 2;
             playerRigidBody.velocity = -gameObject.transform.right * 7;
+            animator.runtimeAnimatorController = leftAnim as RuntimeAnimatorController;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             if (isGrounded)
             {
                 playerRigidBody.velocity = gameObject.transform.up * 40;
+                if (direction == 1)
+                {
+                    animator.runtimeAnimatorController = rightAnim as RuntimeAnimatorController;
+                }
+                if (direction == 2)
+                {
+                    animator.runtimeAnimatorController = leftAnim as RuntimeAnimatorController;
+                }
                 PlayerAudioManagerScript.playerAudioSource.Play();
                 //isGrounded = false;
             }
@@ -73,6 +93,14 @@ public class PlayerScript : MonoBehaviour
         if(Input.anyKey == false)
         {
             Debug.Log("NO KEYS DOWN");
+            if(direction == 1)
+            {
+                animator.runtimeAnimatorController = stoppedRight as RuntimeAnimatorController;
+            }
+            if(direction == 2)
+            {
+                animator.runtimeAnimatorController = stoppedLeft as RuntimeAnimatorController;
+            }
         }
 
         CheckPlayerHealth();
