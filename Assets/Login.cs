c0c0bf6af8acabb85e46ecc46cd3f4b7bc842074
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-04-20 22:23:18 +0100

    login

---
+++ Assets/Login.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    public InputField unameIF;
+    public InputField passIF;
+
+    public Text error;
+
+    [System.Serializable]
+    public class DataToSend
+    {
+        public string uname;
+        public string pass;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void UserLogin()
+    {
+        DataToSend userData = new DataToSend();
+        userData.uname = unameIF.text;
+        userData.pass = passIF.text;
+        string jsonData = JsonUtility.ToJson(userData);
+
+        StartCoroutine(PostRequestJSON("https://vesta.uclan.ac.uk/~diqbal/UnityScripts/login.php", jsonData));
+    }
+
+    IEnumerator PostRequestJSON(string url, string json)
+    {
+        var uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        yield return uwr.SendWebRequest();
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("error sending request");
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            if(uwr.downloadHandler.text == "user already exists")
+            {
+                error.text = "Select a unique username or Enter the correct password";
+            }
+            else if(uwr.downloadHandler.text == "done")
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PHPScripts/login.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "diqbal";
+$password = "QhlX6501";
+$dbname = "diqbal";
+
+$json = file_get_contents("php://input");
+
+$data = json_decode($json);
+
+$dbconn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$s = $dbconn->query("SELECT id, password FROM UnityLogin WHERE username='$data->uname'", PDO::FETCH_ASSOC);
+
+if($s->rowCount() > 0){
+    while($row = $s->fetch()){
+        if($data->pass == $row['password']){
+            echo "done";
+        }else{
+            echo "user already exists";
+        }
+        //echo $row['id'];
+    }
+}else if($s->rowCount() == 0){
+    $sql = "INSERT INTO UnityLogin (username, password) VALUES ('$data->uname', '$data->pass')";
+
+    $dbconn->exec($sql);
+    echo "done";
+}
+?>
